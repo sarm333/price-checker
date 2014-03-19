cf27c012cb758bd91bbbfce74cb1e1b387d357f8
Sarm Boonlertuthai <sarm333@gmail.com>
2014-03-19 21:59:59 +0000

    Added inapt browser plugin and buy now button opens the browser

--- www/js/ProductView.js
+++ www/js/ProductView.js
@@ -2,12 +2,18 @@ var ProductView = function(adapter, productPage, product) {
  
     this.initialize = function() {
         this.el = $('<div/>');
+        this.el.on('click', '.buy-now-button', this.clickBuyNow);
     };
     
     this.render = function() {
 	    this.el.html(productPage(product));
 	    return this;
 	};
+
+    this.clickBuyNow = function() {
+        var productUrl = $(".buy-now-button").attr("url");
+        var ref = window.open(productUrl, '_blank', 'location=yes,transitionstyle=crossdissolve');
+    }
 	
     this.initialize();
  
